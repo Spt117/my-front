1e2420641b5b44dff50ac8e79dee02655a5406a6
jb <theshitcoinert@gmail.com>
2025-08-16 00:57:56 +0200

    test déploiement

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/library/providers/Providers";
 import BackendProvider from "@/library/providers/BackendProvider";
+export const dynamic = "force-dynamic";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
