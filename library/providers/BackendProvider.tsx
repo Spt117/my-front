e0893b119419853cd15bf2d2518cb60a65d79e57
Jean-Baptise Fund <theshitcoinert@gmail.com>
2025-08-16 14:09:37 +0200

    maj

--- library/providers/BackendProvider.tsx
+++ library/providers/BackendProvider.tsx
@@ -1,11 +1,14 @@
+import { getServerSession } from "next-auth";
 import { getUserFromHeader } from "../auth/secureServer";
 import { userEmail } from "../utils/uri";
 import StoreProvider from "./StoreProvider";
+import { authOptions } from "../auth/authOption";
 
 export default async function BackendProvider({ children }: Readonly<{ children: React.ReactNode }>) {
     const user = await getUserFromHeader();
+    const session = await getServerSession(authOptions);
 
-    if (!user || user?.email === userEmail) return <StoreProvider user={user}>{children}</StoreProvider>;
+    if (!session || session.user?.email === userEmail) return <StoreProvider user={user}>{children}</StoreProvider>;
 
     return null;
 }
