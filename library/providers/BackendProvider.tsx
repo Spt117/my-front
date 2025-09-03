38619f35188eecf1d0db7d3fca2b2ec6eb63e1be
jb <theshitcoinert@gmail.com>
2025-09-03 17:41:33 +0200

    test amazon de ajouté

--- library/providers/BackendProvider.tsx
+++ library/providers/BackendProvider.tsx
@@ -1,38 +1,41 @@
-import { getServerSession } from "next-auth";
-import { userEmail } from "../utils/uri";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
-import { authOptions } from "../auth/authOption";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { getServerSession } from "next-auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { authOptions } from "../auth/authOption";
+import { userEmail } from "../utils/uri";
 
 export default async function BackendProvider({ children }: Readonly<{ children: React.ReactNode }>) {
     const session = await getServerSession(authOptions);
     const headersList = await headers();
     const pathname = headersList.get("x-pathname") || "/unknown";
+    // Redirection côté serveur si pas de session et pas sur la page boarding
+    if (!session && pathname !== "/boarding") redirect("/boarding");
 
     if (!session) {
         if (pathname !== "/boarding") return null;
         return <>{children}</>;
     }
 
-    if (session.user?.email === userEmail)
-        return (
-            <SidebarProvider
-                style={
-                    {
-                        "--sidebar-width": "calc(var(--spacing) * 72)",
-                        "--header-height": "calc(var(--spacing) * 12)",
-                    } as React.CSSProperties
-                }
-            >
-                <AppSidebar variant="inset" />
-                <SidebarInset>
-                    <SiteHeader />
-                    {children}
-                </SidebarInset>
-            </SidebarProvider>
-        );
+    if (session && pathname === "/boarding") redirect("/");
 
-    return null;
+    if (session.user?.email !== userEmail) return null;
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                {children}
+            </SidebarInset>
+        </SidebarProvider>
+    );
 }
--- library/providers/Providers.tsx
+++ library/providers/Providers.tsx
@@ -1,13 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import RouterProvider from "./RouterProvider";
 
 export default function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <>
-            <SessionProvider>
-                <RouterProvider>{children}</RouterProvider>
-            </SessionProvider>
-        </>
-    );
+    return <SessionProvider>{children}</SessionProvider>;
 }
