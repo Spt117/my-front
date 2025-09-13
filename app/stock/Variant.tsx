1824a0d70712a80b8152f81f0079503f4187699b
Jean-Baptise Fund <theshitcoinert@gmail.com>
2025-09-13 19:54:25 +0200

    maj stocks

--- app/stock/Variant.tsx
+++ app/stock/Variant.tsx
@@ -2,9 +2,17 @@ import { toggleRebuy, toggleRebuyLater } from "@/library/models/produits/middlew
 import { TVariant } from "@/library/models/produits/Variant";
 import useVariantStore from "./store";
 import { Switch } from "@/components/ui/switch";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { postServer } from "@/library/utils/fetchServer";
+import { toast } from "sonner";
+import { useState } from "react";
+import { Spinner } from "@/components/ui/shadcn-io/spinner/index";
 
 export function Variant({ variant }: { variant: TVariant }) {
     const { setVariantsBuy, setVariantsBuyLater } = useVariantStore();
+    const [numberInput, setNumberInput] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleRebuyChange = async () => {
         const data = await toggleRebuy(variant.sku, !variant.rebuy);
@@ -22,21 +30,63 @@ export function Variant({ variant }: { variant: TVariant }) {
         setVariantsBuyLater(variantsBuyLater);
     };
 
+    const ids = variant.ids;
+    const id = ids.find((id) => id.shop === "bayblade-shops.myshopify.com")?.idProduct;
+    const url = `https://bayblade-shops.myshopify.com/admin/products/${id?.replace("gid://shopify/Product/", "")}`;
+
+    const handleUpdateVariantStock = async () => {
+        if (numberInput <= 0) {
+            toast.error("Veuillez entrer une quantité valide");
+            return;
+        }
+        setIsLoading(true);
+        const url = `http://localhost:9100/shopify/update-stock`;
+        const data = {
+            domain: "bayblade-shops.myshopify.com",
+            sku: variant.sku,
+            quantity: numberInput,
+        };
+        console.log(data);
+
+        const res = await postServer(url, data);
+        if (res.error) toast.error("Erreur lors de la mise à jour du stock");
+        if (res.message) toast.success(res.message);
+        setIsLoading(false);
+        setNumberInput(0);
+    };
+
     return (
         <div className="border-b border-gray-300 p-4">
             <h2 className="text-lg font-semibold">{variant.title}</h2>
-            <p className="text-sm text-gray-600">SKU: {variant.sku}</p>
-            <div className="flex items-center align-center rounded">
-                <p className="text-sm text-gray-600 w-1/4">Rebuy:</p>
-                <div>
-                    <Switch checked={variant.rebuy} onCheckedChange={handleRebuyChange} className="mt-2" />
+            <a href={url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                <p className="text-sm text-gray-600">SKU: {variant.sku}</p>
+            </a>
+            <div>
+                <div className="flex items-center align-center rounded">
+                    <p className="text-sm text-gray-600 w-50">Rebuy:</p>
+                    <div>
+                        <Switch checked={variant.rebuy} onCheckedChange={handleRebuyChange} className="mt-2" />
+                    </div>
+                </div>
+                <div className="flex items-center align-center rounded">
+                    <p className="text-sm text-gray-600 w-50">Rebuy Later:</p>
+                    <div>
+                        <Switch checked={variant.rebuyLater} onCheckedChange={handleRebuyLaterChange} className="mt-2" />
+                    </div>
                 </div>
             </div>
-            <div className="flex items-center align-center rounded">
-                <p className="text-sm text-gray-600 w-1/4">Rebuy Later:</p>
-                <div>
-                    <Switch checked={variant.rebuyLater} onCheckedChange={handleRebuyLaterChange} className="mt-2" />
-                </div>
+            <div className="mt-4 flex space-x-2">
+                <Input
+                    value={numberInput}
+                    placeholder="Quantité"
+                    type="number"
+                    className="w-min"
+                    onChange={(e) => {
+                        setNumberInput(Number(e.target.value));
+                    }}
+                />
+                {!isLoading && <Button onClick={handleUpdateVariantStock}>Ajouter</Button>}
+                {isLoading && <Spinner className="h-6 w-6" />}{" "}
             </div>
         </div>
     );
