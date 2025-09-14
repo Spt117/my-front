0738406f6c55f35144983d07e2c1e50c9d841839
jb <theshitcoinert@gmail.com>
2025-09-14 18:19:15 +0200

    maj product search

--- components/header/site-header.tsx
+++ components/header/site-header.tsx
@@ -9,7 +9,7 @@ export async function SiteHeader() {
     const headersList = await headers();
     const pathname = headersList.get("x-pathname") || "/unknown";
 
-    const searchProductsPaths = ["/product-duplicate", "/product"];
+    const searchProductsPaths = ["/product-duplicate", "/product", "/product-create", "/stock"];
 
     return (
         <header className="sticky top-0 z-50 bg-white flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
