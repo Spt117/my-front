53e8631acebcb6a93deac550adabea25d90fc1e7
Jean-Baptise Fund <theshitcoinert@gmail.com>
2025-09-14 20:33:52 +0200

    maj server

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,7 +1,5 @@
 import { authOptions } from "@/library/auth/authOption";
-import { getServer } from "@/library/utils/fetchServer";
 import { getServerSession } from "next-auth";
-import DataFront from "./frontServer";
 import ScanVeille from "./ScanVeille";
 
 export interface dataStock {
@@ -13,13 +11,5 @@ export default async function Page() {
     const session = await getServerSession(authOptions);
     if (!session) return null;
 
-    const url = `http://localhost:9100/stock`;
-    const res = (await getServer(url)) as dataStock[];
-
-    return (
-        <div className="@container/main flex flex-1 flex-col gap-4 p-4 md:p-6">
-            <ScanVeille />
-            {res.length > 0 && <DataFront data={res} />}
-        </div>
-    );
+    return <ScanVeille />;
 }
