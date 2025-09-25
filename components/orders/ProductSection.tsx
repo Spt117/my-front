99e99c9680e382912ad2f34c84b1175159c2c980
jb <theshitcoinert@gmail.com>
2025-09-25 16:06:11 +0200

    maj order variant

--- components/orders/ProductSection.tsx
+++ components/orders/ProductSection.tsx
@@ -9,25 +9,33 @@ export default function ProductSection({ node, domain }: { node: LineItemNode; d
     const productAdminUrl = `https://${domain}/admin/products/${idProduct}`;
     const boutique = boutiqueFromDomain(domain);
 
-    return (
-        <a href={productAdminUrl} target="_blank" rel="noopener noreferrer" className="">
-            <div className="flex items-center gap-2 border-2 border-gray-200 rounded-md p-1 hover:shadow-md transition-shadow">
-                <div className="relative w-24 h-24 z-0">
-                    <Image sizes="50" priority src={node.variant?.product.featuredImage.url || "/no_image.png"} alt={node.title} fill className="object-cover rounded-md" />
+    if (node.variant)
+        return (
+            <a href={productAdminUrl} target="_blank" rel="noopener noreferrer" className="">
+                <div className="flex items-center gap-2 border-2 border-gray-200 rounded-md p-1 hover:shadow-md transition-shadow">
+                    <div className="relative w-24 h-24 z-0">
+                        <Image
+                            sizes="50"
+                            priority
+                            src={node.variant?.product.featuredImage.url || "/no_image.png"}
+                            alt={node.title}
+                            fill
+                            className="object-cover rounded-md"
+                        />
+                    </div>
+                    <div>
+                        <p className="text-sm font-medium">{node.title}</p>
+                        <p className="text-xs text-gray-500">{`${node.variant.price} ${boutique.devise}`}</p>
+                        <p className="text-xs text-gray-500">SKU: {node.sku}</p>
+                        {node.quantity === 1 && <p className="text-xs text-gray-500">Quantité: {node.quantity}</p>}
+                        {node.quantity > 1 && (
+                            <p className="text-sm text-red-500 font-bold flex items-center gap-1">
+                                <AlertTriangle />
+                                Quantités: {node.quantity}
+                            </p>
+                        )}
+                    </div>
                 </div>
-                <div>
-                    <p className="text-sm font-medium">{node.title}</p>
-                    <p className="text-xs text-gray-500">{`${node.variant.price} ${boutique.devise}`}</p>
-                    <p className="text-xs text-gray-500">SKU: {node.sku}</p>
-                    {node.quantity === 1 && <p className="text-xs text-gray-500">Quantité: {node.quantity}</p>}
-                    {node.quantity > 1 && (
-                        <p className="text-sm text-red-500 font-bold flex items-center gap-1">
-                            <AlertTriangle />
-                            Quantités: {node.quantity}
-                        </p>
-                    )}
-                </div>
-            </div>
-        </a>
-    );
+            </a>
+        );
 }
