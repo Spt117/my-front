39648012c41f434314646e070da5652a471f6c6f
jb <theshitcoinert@gmail.com>
2025-09-30 14:53:48 +0200

    début travail beyblade

--- app/product/Product.tsx
+++ app/product/Product.tsx
@@ -1,16 +1,14 @@
 "use client";
 import useShopifyStore from "@/components/shopify/shopifyStore";
 import { useEventListener } from "@/library/hooks/useEvent/useEvents";
-import { getVariantBySku } from "@/library/models/produits/middlewareVariants";
 import { TVariant } from "@/library/models/produits/Variant";
 import { boutiqueFromDomain, IShopify } from "@/library/params/paramsShopify";
 import { ProductGET } from "@/library/types/graph";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
-import { getProduct } from "../../components/shopify/serverActions";
 import { ResponseServer } from "../../components/shopify/typesShopify";
 import ProductContent from "./ProductContent";
-import { useRouter } from "next/navigation";
 
 export default function Product({
     productData,
@@ -32,11 +30,6 @@ export default function Product({
         if (variantData) setVariant(variantData);
         if (productData.error) toast.error(productData.error);
     }, [shopify.domain, productData.response, variantData]);
-
-    useEffect(() => {
-        console.log("Variant Data ", variantData);
-        console.log("Variant Store ", variant);
-    }, [variantData, variant]);
 
     const getProductUpdated = async (sku: string) => {
         const currentVariant = useShopifyStore.getState().variant;
