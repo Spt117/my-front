0901bc66c3f132d9685550d8673662b40ae2e52a
jb <theshitcoinert@gmail.com>
2025-10-20 21:35:03 +0200

    maj

--- library/models/produits/middlewareVariants.ts
+++ library/models/produits/middlewareVariants.ts
@@ -1,35 +1,35 @@
-"use server";
+// "use server";
 
-import { TDomainsShopify } from "@/params/paramsShopify";
-import { TVariant } from "../variantShopify/Variant";
-import { variantController } from "../variantShopify/variantController";
+// import { TDomainsShopify } from "@/params/paramsShopify";
+// import { TVariant } from "../variantShopify/Variant";
+// import { variantController } from "../variantShopify/variantController";
 
-export async function getStockVariant(domain: TDomainsShopify) {
-    const data = await variantController(domain).getVariantRebuy();
-    return data;
-}
+// export async function getStockVariant(domain: TDomainsShopify) {
+//     const data = await variantController(domain).getVariantRebuy();
+//     return data;
+// }
 
-export async function getVariantBySku(domain: TDomainsShopify, sku: string) {
-    const data = await variantController(domain).getVariantBySku(sku);
-    return data;
-}
+// export async function getVariantBySku(domain: TDomainsShopify, sku: string) {
+//     const data = await variantController(domain).getVariantBySku(sku);
+//     return data;
+// }
 
-export async function toggleRebuy(domain: TDomainsShopify, sku: string, rebuy: boolean) {
-    const data = await variantController(domain).rebuyBySku(sku, rebuy);
-    return data;
-}
+// export async function toggleRebuy(domain: TDomainsShopify, sku: string, rebuy: boolean) {
+//     const data = await variantController(domain).rebuyBySku(sku, rebuy);
+//     return data;
+// }
 
-export async function toggleRebuyLater(domain: TDomainsShopify, sku: string, rebuyLater: boolean) {
-    if (rebuyLater) await variantController(domain).rebuyBySku(sku, false);
-    const data = await variantController(domain).rebuyLaterBySku(sku, rebuyLater);
-    return data;
-}
+// export async function toggleRebuyLater(domain: TDomainsShopify, sku: string, rebuyLater: boolean) {
+//     if (rebuyLater) await variantController(domain).rebuyBySku(sku, false);
+//     const data = await variantController(domain).rebuyLaterBySku(sku, rebuyLater);
+//     return data;
+// }
 
-export async function toggleBought(domain: TDomainsShopify, variant: TVariant, bought: boolean) {
-    const data = await variantController(domain).boughtBySku(variant.sku, bought);
-    if (bought) {
-        if (variant.rebuy) await variantController(domain).rebuyBySku(variant.sku, false);
-        if (variant.rebuyLater) await variantController(domain).rebuyLaterBySku(variant.sku, false);
-    }
-    return data;
-}
+// export async function toggleBought(domain: TDomainsShopify, variant: TVariant, bought: boolean) {
+//     const data = await variantController(domain).boughtBySku(variant.sku, bought);
+//     if (bought) {
+//         if (variant.rebuy) await variantController(domain).rebuyBySku(variant.sku, false);
+//         if (variant.rebuyLater) await variantController(domain).rebuyLaterBySku(variant.sku, false);
+//     }
+//     return data;
+// }
