5ef21f27aa3a1df0cd8af2594e7fe03c68f20fbf
jb <theshitcoinert@gmail.com>
2026-01-10 22:16:43 +0100

    ok

--- components/menu/NavShopify.tsx
+++ components/menu/NavShopify.tsx
@@ -2,13 +2,14 @@
 
 import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu } from "@/components/ui/sidebar";
 import { menuShopify } from "@/params/menu";
-import { Eye } from "lucide-react";
+import { Eye, LayoutDashboard } from "lucide-react";
 import useShopifyStore from "../shopify/shopifyStore";
 import Menu from "./menu";
 
 export function NavShopify() {
     const { shopifyBoutique } = useShopifyStore();
     const productUrl = `https://${shopifyBoutique?.publicDomain}`;
+    const adminUrl = `https://${shopifyBoutique?.domain}/admin`;
 
     const menuShopifyItems = menuShopify(shopifyBoutique?.id);
 
@@ -19,15 +20,26 @@ export function NavShopify() {
                     <div className="flex items-center justify-between ">
                         <SidebarGroupLabel className="px-3 text-sm font-medium text-muted-foreground">Shopify</SidebarGroupLabel>
                         {shopifyBoutique && (
-                            <a
-                                href={productUrl}
-                                className="p-1 hover:bg-gray-200 rounded-md right-3 top-2"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                title="Afficher votre boutique"
-                            >
-                                <Eye size={20} className="text-slate-400 hover:text-slate-600 transition-colors" />
-                            </a>
+                            <div className="flex items-center gap-1 pr-2">
+                                <a
+                                    href={productUrl}
+                                    className="p-1 hover:bg-gray-200 rounded-md"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    title="Afficher la boutique"
+                                >
+                                    <Eye size={20} className="text-slate-400 hover:text-slate-600 transition-colors" />
+                                </a>
+                                <a
+                                    href={adminUrl}
+                                    className="p-1 hover:bg-gray-200 rounded-md"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    title="Afficher l'interface d'administration"
+                                >
+                                    <LayoutDashboard size={20} className="text-slate-400 hover:text-slate-600 transition-colors" />
+                                </a>
+                            </div>
                         )}
                     </div>
                     {menuShopifyItems.map((item) => (
