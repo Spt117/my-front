956005e9fe02fdbb6b2dfb2b58f4099ffc798bf4
jb <theshitcoinert@gmail.com>
2026-02-10 00:59:47 +0100

    ok

--- app/shopify/[shopId]/draft/layout.tsx
+++ app/shopify/[shopId]/draft/layout.tsx
@@ -3,33 +3,18 @@
 import useShopifyStore from "@/components/shopify/shopifyStore";
 import { useEventListener } from "@/library/hooks/useEvent/useEvents";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
     const { shopifyBoutique } = useShopifyStore();
 
-    useEventListener("products/update", (data) => {
-        console.log("data.domain");
-        console.log(data.domain);
-        console.log("shopifyBoutique.domain");
-        console.log(shopifyBoutique?.domain);
-
+    useEventListener("products/update", () => {
         if (shopifyBoutique?.domain) router.refresh();
     });
 
     useEventListener("products/create", (data) => {
-        console.log("data.domain");
-        console.log(data.domain);
-        console.log("shopifyBoutique.domain");
-        console.log(shopifyBoutique?.domain);
-
         if (data.domain === shopifyBoutique?.domain) router.refresh();
     });
-
-    useEffect(() => {
-        console.log(shopifyBoutique);
-    }, [shopifyBoutique]);
 
     return <>{children}</>;
 }
--- app/shopify/layout.tsx
+++ app/shopify/layout.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import useShopifyStore from "@/components/shopify/shopifyStore";
 import { useEventListener } from "@/library/hooks/useEvent/useEvents";
-import { useRouter } from "next/navigation";
+import { boutiqueFromId, TLocationHome } from "@/params/paramsShopify";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const { setShopifyBoutique } = useShopifyStore();
     const router = useRouter();
+    const param = useParams();
 
     useEventListener("orders/paid", (data: any) => {
         router.refresh();
     });
+
+    useEffect(() => {
+        const shopIdString = Array.isArray(param.shopId) ? param.shopId[0] : param.shopId;
+        if (!shopIdString) return;
+        const idShop = Number(shopIdString) as TLocationHome;
+        const shop = boutiqueFromId(idShop);
+        setShopifyBoutique(shop);
+    }, [param]);
 
     return <>{children}</>;
 }
